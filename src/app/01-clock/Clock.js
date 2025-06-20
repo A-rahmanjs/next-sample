1060b711942e03016e77267eea33493f4de9a154
abdo1code <abdocool23233@gmail.com>
2025-06-20 18:03:02 +0300

    eee

---
+++ src/app/01-clock/Clock.js
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+import format from "date-fns/format";
+
+function Clock() {
+  const [time, setTime] = React.useState();
+
+  React.useEffect(() => {
+    const intervalId = window.setInterval(() => {
+      setTime(new Date());
+    }, 50);
+
+    return () => {
+      window.clearInterval(intervalId);
+    };
+  }, []);
+
+  return <p className="clock">{time ? format(time, "hh:mm:ss.S a") : "___"}</p>;
+}
+
+export default Clock;
--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,5 @@ import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <>
-      <Link href="/about">about</Link>
-    </>
-  );
+  return <>NEXT APP</>;
 }
